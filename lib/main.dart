4506707f20e9693e5852b8fd4f42e5bf83dbaa10
nouh ben kadi <benkadi.nouh@icloud.com>
2020-08-22 23:32:34 +0100

    provider update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:please_work/models/user.dart';
 import 'package:please_work/screens/wrapper.dart';
+import 'package:please_work/services/authentication_service.dart';
 import 'package:please_work/theme.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,10 +16,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: myAppThemeData(),
-      home: Wrapper(),
+    return StreamProvider<CustomUser>.value(
+      value: AuthService().user, //the stream we are listening to
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: myAppThemeData(),
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:please_work/models/user.dart';
 import 'package:please_work/screens/authenticat/authenticat.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _usr = Provider.of<CustomUser>(context);
+    print("Value from the ProviderStream: $_usr");
     return Authenticat();
   }
 }
